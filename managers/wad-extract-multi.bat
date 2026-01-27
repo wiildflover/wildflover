c141888d5442ccb6faf257b350b338a50e8b8b73
wld <46638485+wiildflover@users.noreply.github.com>
2026-01-27 14:57:22 +0300

    Delete managers directory

--- managers/wad-extract-multi.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for %%f in (%1\*.wad.client) do (
-    "%~dp0\wad-extract.exe" "%%f"
-)
-pause
--- managers/wxy-extract-multi.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for %%f in (%1\*.wxy) do (
-    "%~dp0\wxy-extract.exe" "%%f"
-)
-pause
